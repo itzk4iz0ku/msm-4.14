aa8f9f432e9a6be38b6da51efcd4f87bae287469
itzk4iz0ku <phennaux115@gmail.com>
2023-11-21 17:36:27 +0100

    Revert "qcacld-3.0: Discard wlan_boot sysfs code on !CONFIG_MODULES."
    This reverts commit d5a98b4dfcdde5ffda492719bc64036f213b14dd.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -232,7 +232,7 @@ static unsigned int dev_num = 1;
 static struct cdev wlan_hdd_state_cdev;
 static struct class *class;
 static dev_t device;
-#if 0
+#ifndef MODULE
 static struct gwlan_loader *wlan_loader;
 static ssize_t wlan_boot_cb(struct kobject *kobj,
 			    struct kobj_attribute *attr,
@@ -16682,7 +16682,7 @@ static void hdd_driver_unload(void)
 	hdd_qdf_deinit();
 }
 
-#if 0
+#ifndef MODULE
 /**
  * wlan_boot_cb() - Wlan boot callback
  * @kobj:      object whose directory we're creating the link in.
@@ -16808,6 +16808,7 @@ static int wlan_deinit_sysfs(void)
 
 #endif /* MODULE */
 
+#ifdef MODULE
 /**
  * hdd_module_init() - Module init helper
  *
@@ -16822,7 +16823,21 @@ static int hdd_module_init(void)
 
 	return 0;
 }
+#else
+static int __init hdd_module_init(void)
+{
+	int ret = -EINVAL;
 
+	ret = wlan_init_sysfs();
+	if (ret)
+		hdd_err("Failed to create sysfs entry");
+
+	return ret;
+}
+#endif
+
+
+#ifdef MODULE
 /**
  * hdd_module_exit() - Exit function
  *
@@ -16834,6 +16849,13 @@ static void __exit hdd_module_exit(void)
 {
 	hdd_driver_unload();
 }
+#else
+static void __exit hdd_module_exit(void)
+{
+	hdd_driver_unload();
+	wlan_deinit_sysfs();
+}
+#endif
 
 static int fwpath_changed_handler(const char *kmessage,
 				  const struct kernel_param *kp)
